1629c629f170370b6870d97cb81dde654ad3b5c6
roman89ua <roman89ua@gmail.com>
2019-09-27 10:43:44 +0300

    simpl taskes of objectes

---
+++ script.js
@@ -0,0 +1,67 @@
+//Перевірка на пустоту.
+/*var empty = {
+
+},
+notEmpty = {
+    name: "blabla"
+}
+function isEmpty(obj) {
+    for(var k in obj){
+        return false;
+    }
+    return true;
+}
+*/
+
+
+// Обєкт константа
+/*
+const user = {
+    name: "John"
+};
+var user1 = user;
+
+user1.name = "been chenged"
+
+*/
+
+
+/*
+//сума значнь ключів в змінну
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+
+}
+console.log(sum);
+  */
+ 
+// множення числових значень ключів обєкта з перевіркою typeof
+/*
+ let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+ }
+function multiplyNumeric(obj){
+    
+    for (let key in obj) {
+
+        if(typeof obj[key] == "number") {
+           
+            obj[key] *= 2; 
+            
+        }
+        
+    } 
+    
+}
+*/
+
+
+
